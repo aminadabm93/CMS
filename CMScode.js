b6ceace38e8a4d64c91f4cd6d62e6accdead34ee
aminadab.morales <aminadabm93@gmail.com>
2020-10-25 15:51:13 -0500

    adding function to add employees

--- CMScode.js
+++ CMScode.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { connect } = require("http2");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -96,6 +97,7 @@ function addDepartment(){
         var query = "INSERT INTO departments(name) VALUES (?)";
         connection.query(query,answer.department,function(err,res){
             if (err) throw err;
+            console.log("You have succesfully added a department!");
             runSearch();
         });
     });
@@ -104,6 +106,33 @@ function addDepartment(){
 function addEmployee(){
     //inquire employee first, last, roleID, 
     // inquirere if known manager ID then insert
+    var employeeQs = [{
+        message:"What is the employee's first name?",
+        name:"firstName",
+        type:"input"
+    },{
+        message:"What is the employee's last name?",
+        name:"lastName",
+        type:"input"
+    },{
+        message:"What is the employee's role id?",
+        name:"roleID",
+        type:"number"
+    },{
+        message:"If the employee has a manager id, input it. If none, input 0.",
+        name:"managerID",
+        type:"number"
+    }];
+
+    inquirer
+    .prompt(employeeQs).then(function(answer){
+        var query = "INSERT INTO employees(first_name,last_name,role_id,manager_id) VALUES(?,?,?,?)";
+        connection.query(query,[answer.firstName,answer.lastName,answer.roleID,answer.managerID],function(err,res){
+            if (err) throw err;
+            console.log("You have successfuly added an employee!");
+            runSearch();
+        });
+    });
 }
 
 function addRole(){
